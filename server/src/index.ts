3f4fb656ade656173ad6b5675d0eba2777f79377
Adam Kamboj <kamboj.adam@gmail.com>
2021-06-28 22:22:47 -0400

    set up graphql server and typegraphql, created post resolver and updated post entitie work with graphql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,29 @@
-import { Post } from "./entities/Post";
 import { __prod__ } from "./constants";
 import { MikroORM } from "@mikro-orm/core";
+import mikroConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    entities: [Post],
-    dbName: "jaunster2",
-    type: "postgresql",
-    debug: !__prod__,
+  const orm = await MikroORM.init(mikroConfig);
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(9999, () => {
+    console.log("server started on localhost:9999");
   });
 };
 
