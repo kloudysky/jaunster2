09b977fdd3533b8500e0f826e0439aa1c5990142
Adam Kamboj <kamboj.adam@gmail.com>
2021-06-28 21:28:49 -0400

    setting up migrations

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,16 @@
+import { Post } from "./entities/Post";
 import { __prod__ } from "./constants";
 import { MikroORM } from "@mikro-orm/core";
 
 const main = async () => {
   const orm = await MikroORM.init({
+    entities: [Post],
     dbName: "jaunster2",
     type: "postgresql",
     debug: !__prod__,
   });
 };
 
-main();
+main().catch((err) => {
+  console.log(err);
+});
