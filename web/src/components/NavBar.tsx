84cb097b58955a0110ca1ebd99b9863867073078
Adam Kamboj <kamboj.adam@gmail.com>
2021-07-16 13:03:30 -0400

    added and configured navbar for login/register and if user is already logged in

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,17 +1,39 @@
-import { Box, Flex, Link } from "@chakra-ui/react";
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
 import React from "react";
+import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+
+  let body = null;
+
+  if (fetching) {
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Button mr={2}>Login</Button>
+        </NextLink>
+        <NextLink href="/register">
+          <Button>Register</Button>
+        </NextLink>
+      </>
+    );
+  } else {
+    body = (
+      <Flex justifyContent="center" alignItems="center">
+        <Box mr={4}>{data.me.username}</Box>
+        <Button>Logout</Button>
+      </Flex>
+    );
+  }
+
   return (
     <Flex bg="tomato" p={4}>
-      <Box ml={"auto"}>
-        <Link color="white" mr={2}>
-          Login
-        </Link>
-        <Link color="white">Register</Link>
-      </Box>
+      <Box ml={"auto"}>{body}</Box>
     </Flex>
   );
 };
